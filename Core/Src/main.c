ee483c82afcfa7409d621b2791ad227c909efa80
a.shiliaev <andrey.shilyayev.89@mail.ru>
2024-05-29 16:21:53 +0300

    ypr MPU6050 working

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -33,6 +33,7 @@ int __io_putchar(int ch);
 #include "MPU6050_6Axis_MotionApps20.h"
 MPU6050 mpu;
 #define accelgyro mpu
+#define M_PI           3.14159265358979323846  /* pi */
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -87,6 +88,43 @@ uint16_t adc_buff[5][10];
 uint16_t buf[500];
 int buf_itera;
 int ifg;
+//#define OUTPUT_READABLE_QUATERNION
+//#define OUTPUT_READABLE_EULER
+#define OUTPUT_READABLE_YAWPITCHROLL
+//#define OUTPUT_READABLE_REALACCEL
+//#define OUTPUT_READABLE_WORLDACCEL
+//#define OUTPUT_TEAPOT
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+   int16_t ax, ay, az;
+   int16_t gx, gy, gz;
+   int16_t offset = 0;
 /* USER CODE END 0 */
 
 /**
@@ -132,16 +170,81 @@ int main(void)
   setvbuf(stdout, NULL, _IONBF, 0);
   setvbuf(stderr, NULL, _IONBF, 0);
 
+  I2Cdev::init(&hi2c2);
+  mpu.initialize();
+  // verify connection
+  Serial.println("Testing device connections...");
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  mpu.PrintActiveOffsets();
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+  if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        //mpu.CalibrateAccel(6);//old 6
+        //mpu.CalibrateGyro(6);//old 6
+        //mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        //Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));// we set it on hal drivers
+        Serial.println(F(")..."));
+        //attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);// we set it on hal drivers
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {  
+    delay(100);
+    if (!dmpReady) {continue;}
+
+    if (!mpuInterrupt) {continue;}
+
+    mpuInterrupt = false;
+    // read a packet from FIFO
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) 
+    //mpu.getFIFOBytes(fifoBuffer, packetSize);
+    { // Get the Latest packet 
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/M_PI,1);
+        #endif
+    }
+     //accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+     //printf("%d %d %d %d %d %d\n",ax, ay, az, gx, gy, gz);
+     //delay(100);
     //printf("Display Status: %d %lu\n",HAL_ADC_PollForConversion(&hadc1, 1000),hadc1.Instance->DR);
-    printf("TMR: %lu\n",micros());
-    __asm("nop");
-    HAL_Delay(30);
+    //printf("TMR: %lu\n",micros());
+   // __asm("nop");
+   // HAL_Delay(30);
    // while(buf_itera < 500)
     //{
     //  if(HAL_ADC_PollForConversion(&hadc1, 1000) == HAL_OK)
@@ -578,6 +681,10 @@ int __io_putchar(int ch)
 {
   HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 10);
   return ch;
+}
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  dmpDataReady();
 }
 /* USER CODE END 4 */
 
