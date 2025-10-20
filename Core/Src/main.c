a563910db9dc8029a29ac7826ff2b683321b5808
a.shiliaev <andrey.shilyayev.89@mail.ru>
2025-10-20 14:52:47 +0300

    запустил LTDC, загружает из SRAM 0x2000000

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -63,6 +63,8 @@ ADC_HandleTypeDef hadc1;
 
 I2C_HandleTypeDef hi2c3;
 
+LTDC_HandleTypeDef hltdc;
+
 SPI_HandleTypeDef hspi5;
 
 TIM_HandleTypeDef htim7;
@@ -82,6 +84,7 @@ static void MX_SPI5_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_TIM7_Init(void);
 static void MX_UART5_Init(void);
+static void MX_LTDC_Init(void);
 static void MX_I2C3_Init(void);
 /* USER CODE BEGIN PFP */
 
@@ -126,10 +129,11 @@ __enable_irq();
   MX_ADC1_Init();
   MX_TIM7_Init();
   MX_UART5_Init();
+  MX_LTDC_Init();
   MX_I2C3_Init();
   /* USER CODE BEGIN 2 */
   //MPU6050_Initialize();
-#ifdef LCD_SPI
+#ifndef LCD_SPI
   ili9341_Init_direct();
   int rotation = 4;
   while(1){
@@ -140,7 +144,11 @@ __enable_irq();
       HAL_Delay(2000);
     }
   } 
-#else
+#endif
+#ifdef LCD_TRAINING
+ili9341_Init();
+#endif
+#ifndef LCD_BSP_EXAMPLE
     /* Initialize the LCD */
   BSP_LCD_Init();
   /* Initialize the LCD Layers */
@@ -315,6 +323,68 @@ static void MX_I2C3_Init(void)
   /* USER CODE BEGIN I2C3_Init 2 */
 
   /* USER CODE END I2C3_Init 2 */
+
+}
+
+/**
+  * @brief LTDC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_LTDC_Init(void)
+{
+
+  /* USER CODE BEGIN LTDC_Init 0 */
+
+  /* USER CODE END LTDC_Init 0 */
+
+  LTDC_LayerCfgTypeDef pLayerCfg = {0};
+
+  /* USER CODE BEGIN LTDC_Init 1 */
+
+  /* USER CODE END LTDC_Init 1 */
+  hltdc.Instance = LTDC;
+  hltdc.Init.HSPolarity = LTDC_HSPOLARITY_AL;
+  hltdc.Init.VSPolarity = LTDC_VSPOLARITY_AL;
+  hltdc.Init.DEPolarity = LTDC_DEPOLARITY_AL;
+  hltdc.Init.PCPolarity = LTDC_PCPOLARITY_IPC;
+  hltdc.Init.HorizontalSync = 9;
+  hltdc.Init.VerticalSync = 1;
+  hltdc.Init.AccumulatedHBP = 29;
+  hltdc.Init.AccumulatedVBP = 3;
+  hltdc.Init.AccumulatedActiveW = 269;
+  hltdc.Init.AccumulatedActiveH = 323;
+  hltdc.Init.TotalWidth = 279;
+  hltdc.Init.TotalHeigh = 327;
+  hltdc.Init.Backcolor.Blue = 0;
+  hltdc.Init.Backcolor.Green = 0;
+  hltdc.Init.Backcolor.Red = 0;
+  if (HAL_LTDC_Init(&hltdc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  pLayerCfg.WindowX0 = 0;
+  pLayerCfg.WindowX1 = 240;
+  pLayerCfg.WindowY0 = 0;
+  pLayerCfg.WindowY1 = 320;
+  pLayerCfg.PixelFormat = LTDC_PIXEL_FORMAT_RGB565;
+  pLayerCfg.Alpha = 255;
+  pLayerCfg.Alpha0 = 0;
+  pLayerCfg.BlendingFactor1 = LTDC_BLENDING_FACTOR1_CA;
+  pLayerCfg.BlendingFactor2 = LTDC_BLENDING_FACTOR2_CA;
+  pLayerCfg.FBStartAdress = 0x20000000;
+  pLayerCfg.ImageWidth = 240;
+  pLayerCfg.ImageHeight = 320;
+  pLayerCfg.Backcolor.Blue = 0;
+  pLayerCfg.Backcolor.Green = 0;
+  pLayerCfg.Backcolor.Red = 0;
+  if (HAL_LTDC_ConfigLayer(&hltdc, &pLayerCfg, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN LTDC_Init 2 */
+
+  /* USER CODE END LTDC_Init 2 */
 
 }
 
