3da5bc99bf36d033042e20eae17193b7d8f77e1e
a.shiliaev <andrey.shilyayev.89@mail.ru>
2025-11-05 13:40:35 +0300

    fix modbus client

--- mb_client.py
+++ mb_client.py
@@ -2,12 +2,27 @@
 from pymodbus.client import ModbusSerialClient
 from pymodbus.client import ModbusTcpClient
 import time
+import threading
 
-client = ModbusSerialClient('COM11', baudrate=115200)
+
+def wait_for_enter(stop_event):
+    input("Нажмите Enter для остановки...\n")
+    stop_event.set()  # Устанавливаем событие
+
+client = ModbusSerialClient('COM3', baudrate=115200)
 client.connect()
+
+# Создаем событие для остановки
+stop_event = threading.Event()
+
+# Запускаем поток для ожидания Enter
+stop_thread = threading.Thread(target=wait_for_enter, args=(stop_event,))
+stop_thread.daemon = True
+stop_thread.start()
+
 #client.write_coil(1, True)
-while 1:
+while not stop_event.is_set():
     result = client.read_coils(address=0, count=8) #its, works
     print(result.bits)
-    time.sleep(0.5)
+    stop_event.wait(1)
 client.close()
